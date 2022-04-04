60ef0803d8336736701a947a0b29e526187d24f7
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-04 22:53:09 +0000

    Listen for 'input' events

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+var $inputURL = document.querySelector('#photo-url');
+var $placeholder = document.querySelector('.placeholder');
+$inputURL.addEventListener('input', photoInput);
+
+function photoInput(event) {
+  if ($inputURL.type === 'url') {
+    $placeholder.src = event.target.value;
+  }
+}
