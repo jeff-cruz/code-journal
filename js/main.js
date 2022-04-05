fc22f0cb4a5d4642b60a28f78d8cf93d2075ecb5
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-05 03:25:52 +0000

    Test all functionality again in a private browsing session.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,29 @@ var $placeholder = document.querySelector('.placeholder');
 $inputURL.addEventListener('input', photoInput);
 
 function photoInput(event) {
-  if ($inputURL.type === 'url') {
+  if ($inputURL.value !== '') {
     $placeholder.src = event.target.value;
+  } else {
+    $placeholder.setAttribute('src', 'images/placeholder-image-square.jpg');
   }
+}
+
+var $journalForm = document.querySelector('form');
+$journalForm.addEventListener('submit', buttonClick);
+
+function buttonClick(event) {
+  event.preventDefault();
+  var formTitle = document.querySelector('#title').value;
+  var formPhotoURL = document.querySelector('#photo-url').value;
+  var formNotes = document.querySelector('#notes').value;
+  var entry = {
+    title: formTitle,
+    photoURL: formPhotoURL,
+    notes: formNotes,
+    entryID: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push(entry);
+  $placeholder.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $journalForm.reset();
 }
