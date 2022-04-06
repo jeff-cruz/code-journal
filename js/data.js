f13f3ad94a12d515e77d1cfb65aeeded90c3318d
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-06 04:43:43 +0000

    Ensure that submitting a new journal entry automatically shows the entries view without reloading the page.

--- js/data.js
+++ js/data.js
@@ -18,39 +18,3 @@ function beforeUnload() {
   localStorage.setItem('javascript-local-storage', entriesJSON);
   return entriesJSON;
 }
-
-function renderEntry(entry) {
-  var $li = document.createElement('li');
-
-  var $row = document.createElement('div');
-  $row.setAttribute('class', 'row');
-  $li.appendChild($row);
-
-  var $img = document.createElement('img');
-  $img.setAttribute('class', 'placeholder column-half padding-bottom');
-  $img.setAttribute('src', entry.photoURL);
-  $row.appendChild($img);
-
-  var $columnHalf = document.createElement('div');
-  $columnHalf.setAttribute('class', 'column-half');
-  $row.appendChild($columnHalf);
-
-  var $title = document.createElement('h1');
-  $title.textContent = entry.title;
-  $columnHalf.appendChild($title);
-
-  var $notes = document.createElement('p');
-  $notes.textContent = entry.notes;
-  $columnHalf.appendChild($notes);
-
-  return $li;
-}
-
-window.addEventListener('DOMContentLoaded', loopOverListOfEntries);
-
-var $ul = document.querySelector('.entry-list');
-function loopOverListOfEntries(entry) {
-  for (var i = 0; i < data.entries.length; i++) {
-    $ul.append(renderEntry(data.entries[i]));
-  }
-}
--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 /* exported data */
 var $inputURL = document.querySelector('#photo-url');
 var $placeholder = document.querySelector('.placeholder');
+var $formPage = document.querySelector('.form-page');
+var $entriesPage = document.querySelector('.entries-page');
+var $journalForm = document.querySelector('form');
 $inputURL.addEventListener('input', photoInput);
 
 function photoInput(event) {
@@ -12,7 +15,6 @@ function photoInput(event) {
   }
 }
 
-var $journalForm = document.querySelector('form');
 $journalForm.addEventListener('submit', buttonClick);
 
 function buttonClick(event) {
@@ -30,15 +32,59 @@ function buttonClick(event) {
   data.entries.unshift(entry);
   $placeholder.setAttribute('src', 'images/placeholder-image-square.jpg');
   $journalForm.reset();
+  $entryList.prepend(renderEntry(entry));
+  $formPage.className = 'form-page hidden';
+  $entriesPage.className = 'entries-page view';
+}
+
+function renderEntry(entry) {
+  var $li = document.createElement('li');
+
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
+  $li.appendChild($row);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('class', 'placeholder column-half padding-bottom');
+  $img.setAttribute('src', entry.photoURL);
+  $row.appendChild($img);
+
+  var $columnHalf = document.createElement('div');
+  $columnHalf.setAttribute('class', 'column-half');
+  $row.appendChild($columnHalf);
+
+  var $title = document.createElement('h1');
+  $title.textContent = entry.title;
+  $columnHalf.appendChild($title);
+
+  var $notes = document.createElement('p');
+  $notes.textContent = entry.notes;
+  $columnHalf.appendChild($notes);
+
+  return $li;
+}
+
+window.addEventListener('DOMContentLoaded', loopOverListOfEntries);
+
+var $entryList = document.querySelector('.entry-list');
+function loopOverListOfEntries(entry) {
+  for (var i = 0; i < data.entries.length; i++) {
+    $entryList.append(renderEntry(data.entries[i]));
+  }
 }
 
 var $entriesButton = document.querySelector('.entries-link');
 $entriesButton.addEventListener('click', entriesClick);
 
-var $formPage = document.querySelector('.form-page');
-var $entriesPage = document.querySelector('.entries-page');
 function entriesClick(event) {
-  $entriesButton.className = 'entries-link';
   $formPage.className = 'form-page hidden';
   $entriesPage.className = 'entries-page view';
+}
+
+var $newEntryButton = document.querySelector('.new-entry');
+$newEntryButton.addEventListener('click', newEntryClick);
+
+function newEntryClick(event) {
+  $formPage.className = 'form-page view';
+  $entriesPage.className = 'entries-page hidden';
 }
